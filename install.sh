469e2758ed9a35930cca9b91eb1cd7e888f99593
Aditya Narayan Sarangi <a.n.sarangi@gmail.com>
2023-06-29 18:04:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,19 +6,29 @@ INSTDIR="$( cd "$(dirname "$0")" ; pwd)"
 cd ${INSTDIR}
 chmod 764 squat.sh
 
+if [ -d bwa ]; then
+	rm -r bwa
+fi
+
 if [ -d quast ]; then
 	rm -r quast
 fi
 
+#install bwa
+echo "install bwa-0.7.15"
+wget https://downloads.sourceforge.net/project/bio-bwa/bwa-0.7.15.tar.bz2
+tar jxvf bwa-0.7.15.tar.bz2
+rm bwa-0.7.15.tar.bz2*
+mv bwa-0.7.15 bwa
+cd bwa; make
 
 #install quast
 echo "install quast"
 cd ${INSTDIR}
-wget https://github.com/ablab/quast/releases/download/quast_5.2.0/quast-5.2.0.tar.gz
-tar -xzf quast-5.2.0.tar.gz
-mv quast-5.2.0 quast;
-rm quast-5.2.0.tar.gz
-cp ${INSTDIR}/library/quast_code/* ${INSTDIR}/quast/quast_libs
+git clone https://github.com/ablab/quast.git
+cd quast
+chmod 755 install.sh
+./install.sh
 
 #build readQdist
 echo "build files for generating pre-assembly quality report"
